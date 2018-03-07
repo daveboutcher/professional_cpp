4996857ee7a10d933006b9f92f1fc8c3d2280ec1
Dave Boutcher <dboutcher@ocient.com>
2018-03-07 16:38:32 -0600

    Start of exercise 2

--- Makefile
+++ Makefile
@@ -1,6 +1,14 @@
+# Assume a C++17 (aka c++1z) compiler
 CXXFLAGS = -Wall -std=c++1z -g
+
+# Need to link against the SDL libraries
 LDLIBS = -lSDL2 -lSDL2_image
+
+# Because this is a C++ application, tell make to use the C++ linker
+LINK.o = $(LINK.cc)
 
 all: cards
 
-cards: cards.cpp
+cards: cards.o
+
+cards.o: cards.cpp $(wildcard cdl/*.h)
--- cards.cpp
+++ cards.cpp
@@ -5,6 +5,10 @@
  * International License. Details at 
  * http://creativecommons.org/licenses/by/4.0/
  ***********************************************************************/
+#include <iostream>
+#include "cdl/cdl.h" // includes the "running()" method
+#include "cdl/window.h"
+#include "cdl/surface.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
@@ -13,93 +17,62 @@ int main(int argc, char *argv[])
     // Initialize SDL
     SDL_Init(SDL_INIT_VIDEO);
 
-    // Create a window
-    SDL_Window* window = SDL_CreateWindow("Cards",
-                                          SDL_WINDOWPOS_CENTERED,
-                                          SDL_WINDOWPOS_CENTERED,
-                                          480, // Width
-                                          480, // Height
-                                          SDL_WINDOW_RESIZABLE);
-    
-    // Create a renderer for the window
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,
-                                                SDL_RENDERER_ACCELERATED);
-    
-    // Load the four images
-    SDL_Surface* club_surface = IMG_Load("club.png");
-    SDL_Texture* club_texture = SDL_CreateTextureFromSurface(renderer,
-                                                             club_surface);
-    
-    SDL_Surface* diamond_surface = IMG_Load("diamond.png");
-    SDL_Texture* diamond_texture = SDL_CreateTextureFromSurface(renderer,
-                                                                diamond_surface);
-    
-    SDL_Surface* heart_surface = IMG_Load("heart.png");
-    SDL_Texture* heart_texture = SDL_CreateTextureFromSurface(renderer,
-                                                              heart_surface);
-    
-    SDL_Surface* spade_surface = IMG_Load("spade.png");
-    SDL_Texture* spade_texture = SDL_CreateTextureFromSurface(renderer,
-                                                              spade_surface);
+	try {
 
-    // This is the main SDL loop
-    while(true) {
-        // Loop till we get told to quit
-        SDL_Event event;
-        SDL_PollEvent(&event);
-        if(event.type == SDL_QUIT) {
-            break;
-        }
+		cdl::Window window = cdl::Window("Cards",
+										 cdl::position{SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED},
+										 cdl::size{480, 480});
 
-        // Clear the renderer
-        SDL_RenderClear(renderer);
-
-		// Get the window width
-		int window_width = 0;
-		SDL_GetWindowSize(window, &window_width, nullptr);
-
-		// We are going to display 4 images, with padding between them and
-		// on the side...that works out to 5 blocks of padding
-		int padding = ((window_width -
-						(club_surface->w + diamond_surface->w +
-						 heart_surface->w + spade_surface->w))
-					   / 5);
-					   
-		// Figure out everyone's positions
-		SDL_Rect club_pos =    {padding, 10,
-								club_surface->w, club_surface->h};
-		SDL_Rect diamond_pos = {padding + club_pos.x + club_pos.w, 10,
-								diamond_surface->w, diamond_surface->h};
-		SDL_Rect heart_pos =   {padding + diamond_pos.x + diamond_pos.w, 10,
-								heart_surface->w, heart_surface->h};
-		SDL_Rect spade_pos =   {padding + heart_pos.x + heart_pos.w, 10,
-								spade_surface->w, spade_surface->h};
+		cdl::Renderer renderer = window.createRenderer();
 		
-        // Copy in our images
-        SDL_RenderCopy(renderer, club_texture, nullptr, &club_pos);
-        SDL_RenderCopy(renderer, diamond_texture, nullptr, &diamond_pos);
-        SDL_RenderCopy(renderer, heart_texture, nullptr, &heart_pos);
-        SDL_RenderCopy(renderer, spade_texture, nullptr, &spade_pos);
+		// Load the four images
+		cdl::Surface club_surface = cdl::Surface("club.png");
+		cdl::Texture club_texture = renderer.createTexture(club_surface);
+		
+		cdl::Surface diamond_surface = cdl::Surface("diamond.png");
+		cdl::Texture diamond_texture = renderer.createTexture(diamond_surface);
+		
+		cdl::Surface heart_surface = cdl::Surface("heart.png");
+		cdl::Texture heart_texture = renderer.createTexture(heart_surface);
+		
+		cdl::Surface spade_surface = cdl::Surface("spade.png");
+		cdl::Texture spade_texture = renderer.createTexture(spade_surface);
+		
+		// This is the main SDL loop
+		while(cdl::running()) {
 
-        // Yay...display the images
-        SDL_RenderPresent(renderer);
-    }
+			renderer.clear();
 
-    // Clean up everything
-    SDL_DestroyTexture(club_texture);
-    SDL_FreeSurface(club_surface);
-    
-    SDL_DestroyTexture(diamond_texture);
-    SDL_FreeSurface(diamond_surface);
-    
-    SDL_DestroyTexture(heart_texture);
-    SDL_FreeSurface(heart_surface);
-    
-    SDL_DestroyTexture(spade_texture);
-    SDL_FreeSurface(spade_surface);
-    
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+			// Get the window width
+			int window_width = window.width();
+			
+			// We are going to display 4 images, with padding between them and
+			// on the side...that works out to 5 blocks of padding
+			int padding = ((window_width -
+							(club_texture.size().width + diamond_texture.size().width +
+							 heart_texture.size().width + spade_texture.size().width))
+						   / 5);
+			
+			int currentX = padding;
+			
+			renderer.add(club_texture, cdl::position{currentX, 10});
+			currentX += club_texture.size().width + padding;
+			
+			renderer.add(diamond_texture, cdl::position{currentX, 10});
+			currentX += diamond_texture.size().width + padding;
+			
+			renderer.add(heart_texture, cdl::position{currentX, 10});
+			currentX += heart_texture.size().width + padding;
+			
+			renderer.add(spade_texture, cdl::position{currentX, 10});
+			currentX += spade_texture.size().width + padding;
+
+			renderer.display();
+		}
+	} catch (const cdl::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
     SDL_Quit();
     return 0;
 }
